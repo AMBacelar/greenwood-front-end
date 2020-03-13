2a2c46f22699fe64a79766930a23d3d414a77f60
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-03-13 21:41:31 +0000

    Layout component set up

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Head from "next/head";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 import styles from "./layout.scss";
 
@@ -30,42 +31,44 @@ const Layout: React.SFC<LayoutProps> = ({
       />
     </Head>
     <header>
-      <Navbar sticky="top">
-        <Navbar.Brand as={"a"} header>
-          Project Name
-        </Navbar.Brand>
-        <Nav className="justify-content-end">
-          <Nav.Item>
-            <Nav.Link as="a">The Catalogue</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as="a">Blog</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as="a">The App</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as="a">Sign In</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as="a">Sign Up</Nav.Link>
-          </Nav.Item>
-        </Nav>
+      <Navbar expand="lg" sticky="top">
+        <Container>
+          <Link passHref href="/">
+            <Navbar.Brand as={"a"} header>
+              Project Name
+            </Navbar.Brand>
+          </Link>
+          <Nav className={styles["nav-right"]}>
+            <Nav.Item>
+              <Link passHref href="/">
+                <Nav.Link>The Catalogue</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link passHref href="/a">
+                <Nav.Link>Blog</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link passHref href="/b">
+                <Nav.Link>The App</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link passHref href="/">
+                <Nav.Link>Sign In</Nav.Link>
+              </Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Link passHref href="/">
+                <Nav.Link>Sign Up</Nav.Link>
+              </Link>
+            </Nav.Item>
+          </Nav>
+        </Container>
       </Navbar>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <span>/</span>
-      <Link href="/a">
-        <a>a</a>
-      </Link>
-      <span>/</span>
-      <Link href="/b">
-        <a>b</a>
-      </Link>
     </header>
     {children}
-    <div className={styles["meta-tags"]}>hello world</div>
     <footer>&copy; 2020 AMBacelar</footer>
   </div>
 );
