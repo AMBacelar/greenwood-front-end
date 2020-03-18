ca6f562959e28c403c30b8c31bf7692fab477fa2
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-03-18 22:58:37 +0000

    section 2 completed

---
+++ components/homepage/sections/Section2.tsx
@@ -0,0 +1,59 @@
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import styles from "./Section2.scss";
+
+type Props = {
+  featuredBlog: {
+    bgImage: string;
+    dateCreated: number;
+    url: string;
+    title: string;
+    blogText: string;
+  };
+};
+
+export default ({ featuredBlog }: Props) => {
+  const articleText = featuredBlog.blogText
+    .split("\n")
+    .map((substring, index) => (
+      <p className={styles["blog-text-body"]} key={index}>
+        {substring}
+      </p>
+    ));
+  return (
+    <div className={styles.wrapper}>
+      <Container>
+        <Row>
+          <Col>
+            <div className={styles.content}>
+              <Row>
+                <Col md={5}>
+                  <img
+                    className={styles["blog-image"]}
+                    src={featuredBlog.bgImage}
+                  />
+                </Col>
+                <Col md={7}>
+                  <div className={styles["blog-text-wrapper"]}>
+                    <p className={styles["blog-text-date-created"]}>
+                      {featuredBlog.dateCreated}
+                    </p>
+                    <h2 className={styles["blog-text-blog-title"]}>
+                      {featuredBlog.title}
+                    </h2>
+                    <article>{articleText}</article>
+                    <button className={styles["blog-text-button"]}>
+                      Read More
+                    </button>
+                  </div>
+                </Col>
+              </Row>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../components/layout/layout";
 import Section1 from "../components/homepage/sections/Section1";
+import Section2 from "../components/homepage/sections/Section2";
 
 const businesses = [
   {
@@ -22,11 +23,18 @@ const businesses = [
       "Vegan meals have never been easier due to the fresh fruit and vegetables home grown by Dagenham Greens"
   }
 ];
+const featuredBlog = {
+  bgImage: "/static/images/blogPostImage.png",
+  dateCreated: 12345,
+  url: "/a",
+  title: "Fresh Stock on Aisle 5",
+  blogText: `How many black owned supermarkets have you seen at the end of your road? Not many? Well say hello to Shade's Hypermarket! A brand new supermarket chain bringing you fresh delicious groceries for a family affordable price. \nCEO and founder, Shade Okumaya has spent the last 3 years researching, planning and developing her new business. "There were a lot of sleepless nights. I had to build everything from scratch. I've always taken a keen interest in the food industry market, and realised there weren't many black owned markets in my city. Seeing this gap in the market gave be the push to pursue". \nShade's Hypermarket is based in Tottenham Hale, right in the heart of the borough. With a fresh....`
+};
 
 export default () => (
   <Layout>
     <Section1 businesses={businesses} />
-    <div>Section 2</div>
+    <Section2 featuredBlog={featuredBlog} />
     <div>Section 3</div>
     <div>Section 4</div>
   </Layout>
