a650136521d6d8c8d6871e747ac93fbdced4fa6d
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-06-03 13:53:46 +0100

    styling updates to the page

--- components/homepage/sections/Section1.tsx
+++ components/homepage/sections/Section1.tsx
@@ -1,9 +1,9 @@
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-import styles from "./Section1.scss";
-import LatestBusinesses from "../latestBusinesses/LatestBusinesses";
+import styles from './Section1.scss';
+import LatestBusinesses from '../latestBusinesses/LatestBusinesses';
 
 export default ({ businesses }: any) => {
   return (
@@ -12,17 +12,20 @@ export default ({ businesses }: any) => {
         <Row>
           <Col xs={12} md={8}>
             <div className={styles.header}>
-              <h1>Black Owned Businesses all in one palce</h1>
-              <p>
-                Access our collection of black owned businesses, each reviewed
-                and rated by you. From small businnesses to large organisations
-              </p>
+              <div className={styles.content}>
+                <h1>Black Owned Businesses all in one palce</h1>
+                <p>
+                  Access our collection of black owned businesses, each reviewed
+                  and rated by you. From small businnesses to large
+                  organisations
+                </p>
+              </div>
               <button>check it out</button>
             </div>
           </Col>
         </Row>
       </Container>
-      <div className={styles["latest-businesses"]}>
+      <div className={styles['latest-businesses']}>
         <LatestBusinesses businesses={businesses} />
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,55 @@
-import React from "react";
-import Layout from "components/layout/layout";
-import Section1 from "components/homepage/sections/Section1";
-import Section2 from "components/homepage/sections/Section2";
-import Section3 from "components/homepage/sections/Section3";
+import React from 'react';
+import Layout from '../components/layout/layout';
+import Section1 from '../components/homepage/sections/Section1';
+import Section2 from '../components/homepage/sections/Section2';
+import Section3 from '../components/homepage/sections/Section3';
 
 const latestBusinesses = [
   {
-    thumbnailImage: "/static/images/johns-bakery-thumb.png",
+    thumbnailImage: '/static/images/johns-bakery-thumb.png',
     displayName: "John's Bakery",
-    slug: "johns-bakery",
+    slug: 'johns-bakery',
     shortDescription:
-      "Located in the heart of East London. John's bakery bakes the freshest bread and pastries"
+      "Located in the heart of East London. John's bakery bakes the freshest bread and pastries",
   },
   {
-    thumbnailImage: "/static/images/fine-trims-thumb.png",
-    displayName: "Fine Trims",
-    slug: "fine-trims",
+    thumbnailImage: '/static/images/fine-trims-thumb.png',
+    displayName: 'Fine Trims',
+    slug: 'fine-trims',
     shortDescription:
-      "A barbershop like no other. We provide high-quality haircuts for all hair types. Our barbers are all qualified."
+      'A barbershop like no other. We provide high-quality haircuts for all hair types. Our barbers are all qualified.',
   },
   {
-    thumbnailImage: "/static/images/dagenham-greens-thumb.png",
-    displayName: "Dagenham Greens",
-    slug: "dagenham-greens",
+    thumbnailImage: '/static/images/dagenham-greens-thumb.png',
+    displayName: 'Dagenham Greens',
+    slug: 'dagenham-greens',
     shortDescription:
-      "Vegan meals have never been easier due to the fresh fruit and vegetables home grown by Dagenham Greens"
-  }
+      'Vegan meals have never been easier due to the fresh fruit and vegetables home grown by Dagenham Greens',
+  },
 ];
 const featuredBlog = {
-  bgImage: "/static/images/blogPostImage.png",
+  bgImage: '/static/images/blogPostImage.png',
   dateCreated: 12345,
-  url: "/a",
-  title: "Fresh Stock on Aisle 5",
-  blogText: `How many black owned supermarkets have you seen at the end of your road? Not many? Well say hello to Shade's Hypermarket! A brand new supermarket chain bringing you fresh delicious groceries for a family affordable price. \nCEO and founder, Shade Okumaya has spent the last 3 years researching, planning and developing her new business. "There were a lot of sleepless nights. I had to build everything from scratch. I've always taken a keen interest in the food industry market, and realised there weren't many black owned markets in my city. Seeing this gap in the market gave be the push to pursue". \nShade's Hypermarket is based in Tottenham Hale, right in the heart of the borough. With a fresh....`
+  url: '/a',
+  title: 'Fresh Stock on Aisle 5',
+  blogText: `How many black owned supermarkets have you seen at the end of your road? Not many? Well say hello to Shade's Hypermarket! A brand new supermarket chain bringing you fresh delicious groceries for a family affordable price. \nCEO and founder, Shade Okumaya has spent the last 3 years researching, planning and developing her new business. "There were a lot of sleepless nights. I had to build everything from scratch. I've always taken a keen interest in the food industry market, and realised there weren't many black owned markets in my city. Seeing this gap in the market gave be the push to pursue". \nShade's Hypermarket is based in Tottenham Hale, right in the heart of the borough. With a fresh....`,
 };
 const featuredBusinesses = [
   {
     ariaLabel: "John's Bakery",
-    businessImage: "/static/images/johns-bakery.png",
-    slug: "johns-bakery"
+    businessImage: '/static/images/johns-bakery.png',
+    slug: 'johns-bakery',
   },
   {
-    businessImage: "/static/images/dagenham-greens.png",
-    ariaLabel: "Dagenham Greens",
-    slug: "dagenham-greens"
+    businessImage: '/static/images/dagenham-greens.png',
+    ariaLabel: 'Dagenham Greens',
+    slug: 'dagenham-greens',
   },
   {
-    businessImage: "/static/images/fine-trims.png",
-    ariaLabel: "Fine Trims",
-    slug: "fine-trims"
-  }
+    businessImage: '/static/images/fine-trims.png',
+    ariaLabel: 'Fine Trims',
+    slug: 'fine-trims',
+  },
 ];
 
 const HomePage = () => (
