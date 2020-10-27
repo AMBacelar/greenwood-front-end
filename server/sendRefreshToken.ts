dcb3cc7d50f5a34e9fa4cb492dc12db14bc56ba9
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-10-27 01:50:13 +0000

    set header change for cookie

--- server/sendRefreshToken.ts
+++ server/sendRefreshToken.ts
@@ -1,5 +1,8 @@
 import { Response } from 'express';
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  res.setHeader('Set-Cookie', 'grnwood-network-refresh=' + token);
+  res.setHeader(
+    'Set-Cookie',
+    `grnwood-network-refresh=${token}; Path=/; HttpOnly; Secure`
+  );
 };
