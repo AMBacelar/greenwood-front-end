a919b15a050cf2d2b8bc3d8bcdfe24875c72b833
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-11-24 23:58:49 +0000

    debug

--- server/googleStrategy.ts
+++ server/googleStrategy.ts
@@ -12,9 +12,12 @@ const allowedOrigins = JSON.parse(process.env.NEXT_PUBLIC_ALLOWED_ORIGINS!);
 
 const cors = Cors({
   origin: (origin, callback) => {
+    console.log(origin, allowedOrigins, allowedOrigins.indexOf(origin));
     // allow requests with no origin
     // (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
+    if (!origin) {
+      return callback(null, true);
+    }
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
       return callback(new Error(msg), false);
