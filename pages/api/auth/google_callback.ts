9ae05202d44d358f2d540d2c90595080475a1c47
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-11-29 14:43:20 +0000

    almost there

--- pages/api/auth/google_callback.ts
+++ pages/api/auth/google_callback.ts
@@ -3,7 +3,6 @@ import withPassport, { passport } from '../../../server/googleStrategy';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   passport.authenticate('google')(req, res, () => {
-    console.log('test?', req.session.redirect);
     const destination = req.session.redirect || '/';
     req.session.redirect = null;
     res.redirector(destination);
--- server/sendRefreshToken.ts
+++ server/sendRefreshToken.ts
@@ -1,6 +1,13 @@
 import { Response } from 'express';
+import { serialize } from 'cookie';
 
 export const sendRefreshToken = (res: Response, token: string) => {
+  console.log(
+    serialize('grnwood-network-refresh', token, {
+      httpOnly: true,
+      sameSite: 'strict',
+    })
+  );
   res.setHeader(
     'Set-Cookie',
     `grnwood-network-refresh=${token}; Path=/; HttpOnly;`
