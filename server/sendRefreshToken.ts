fd5d98f0f16eb726c27c97ece07acc80de9fa4b2
Adilson Bacelar <a.m.bacelar@gmail.com>
2020-11-29 14:51:06 +0000

    let's go && gp

--- server/sendRefreshToken.ts
+++ server/sendRefreshToken.ts
@@ -2,14 +2,11 @@ import { Response } from 'express';
 import { serialize } from 'cookie';
 
 export const sendRefreshToken = (res: Response, token: string) => {
-  console.log(
+  res.setHeader(
+    'Set-Cookie',
     serialize('grnwood-network-refresh', token, {
       httpOnly: true,
       sameSite: 'strict',
     })
-  );
-  res.setHeader(
-    'Set-Cookie',
-    `grnwood-network-refresh=${token}; Path=/; HttpOnly;`
   );
 };
